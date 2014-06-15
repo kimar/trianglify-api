c534bb59c145bc3b0b562ab77902e0a58f6e0e32
Marcus Kida <marcus@kida.io>
2014-06-15 17:49:28 +0200

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+var Trianglify = new require('trianglify');
+var rendersvg = require('rendersvg');
+var fs = require('fs');
+var svg2png = require('svg2png');
+var uuid = require('uuid');
+var S = require('string');
+
+var t = new Trianglify();
+
+router.get('/', function (req, res) {
+  var help = {
+    howto: 'GET /image.svg to retrieve randomly generated svg pattern using trianglify.js. Replace svg by png to get converted png file.'
+  }
+  res.json(help);
+});
+
+router.get('/:filename', function(req, res) {
+
+  var wantsPng = false;
+
+  if (S(req.params.filename).endsWith('png')) {
+    wantsPng = true;
+  }
+
+  if(!wantsPng && !S(req.params.filename).endsWith('svg')) {
+    return res.send(400);
+  }
+
+  var fileId = uuid.v4();
+  var svgFile = '/tmp/' + fileId + '.svg';
+  var pngFile = '/tmp/' + fileId + '.png';
+
+  fs.writeFile(svgFile, t.generate(800, 600).svgString, function (err) {
+    if (err) {
+      console.log('Error occured: ' + err);
+      return res.send(500);
+    }
+
+    if (!wantsPng) {
+      return res.sendfile(svgFile);
+    }
+
+    svg2png(svgFile, pngFile, function (err) {
+        if (err) {
+          console.log('Error occured: ' + err);
+          return res.send(500);
+        }
+        res.sendfile(pngFile);
+    });
+
+  });
+});
+
+module.exports = router;
